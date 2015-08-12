743972a5fa02a6256628fbe6c9eded6f73c36be1
GavisusSeb <sebhavens@sebs-mbp.bigpond>
2015-08-12 18:36:51 +1000

    Initial Commit

---
+++ App Competition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  App Competition
+//
+//  Created by Seb Havens on 12/08/2015.
+//  Copyright © 2015 Gavisus Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ App CompetitionTests/App_CompetitionTests.m
@@ -0,0 +1,39 @@
+//
+//  App_CompetitionTests.m
+//  App CompetitionTests
+//
+//  Created by Seb Havens on 12/08/2015.
+//  Copyright © 2015 Gavisus Games. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface App_CompetitionTests : XCTestCase
+
+@end
+
+@implementation App_CompetitionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
